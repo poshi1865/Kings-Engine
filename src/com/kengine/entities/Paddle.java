b1c37d72488fda6b53bf94d0075bf63151b6d1e5
Naachiket Pant <naachiketpant88@gmail.com>
2022-03-06 22:20:58 +0530

    Change paddle rendering position

--- src/com/kengine/entities/Paddle.java
+++ src/com/kengine/entities/Paddle.java
@@ -9,7 +9,7 @@ public class Paddle {
     public int y;
     public int speed = 10;
     public final int width = 10;
-    public final int height = 200;
+    public final int height = 100;
 
     public Paddle(int x, int y) {
         this.x = x;
@@ -19,8 +19,8 @@ public class Paddle {
     public void render(Graphics2D graphics) {
         //Draw Paddle 1
         graphics.setColor(Color.RED);
-        graphics.fillRect(this.x, 0, 4, Game.HEIGHT);
+        graphics.fillRect(this.x, 0, 2, Game.HEIGHT);
         graphics.setColor(Color.BLACK);
-        graphics.fillRect(this.x, this.y, this.width, this.height);
+        graphics.fillRect(this.x - this.width / 2 + 1, this.y, this.width, this.height);
     }
 }
